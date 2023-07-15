ea824ba6bd7081f4d4f7177ed625b656b32e4325
midyHamdoun <midyan.hamdoun@gmail.com>
2023-07-15 15:57:54 +0300

    Add cursors

--- pkgs/nordzy-cursors/default.nix
+++ pkgs/nordzy-cursors/default.nix
@@ -1,12 +1,12 @@
-{ lib, stdenv, fetchTarball, ... }:
+{ lib, stdenv, fetchzip, ... }:
 
 stdenv.mkDerivation rec {
   pname = "nordzy-cursors";
   version = "0.6.0";
 
-  src = fetchTarball {
+  src = fetchzip {
     url = "https://github.com/alvatip/Nordzy-cursors/releases/download/v0.6.0/Nordzy-cursors.tar.gz";
-    sha256 = "ca0e488f746c0ed997e1d034a9ecbf8462ebd0c2257b7ef0882f0b4f1565a86d";
+    sha256 = "1lbl22z3cxb3yj86j6aqpfvjpih8zfg7vjnkawv1wwdg54y5cxaf";
   };
 
   installPhase = ''
