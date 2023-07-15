c38b521bf35c77d32aca4e0fd3045984ddc6b699
midyHamdoun <midyan.hamdoun@gmail.com>
2023-07-15 06:30:37 +0300

    Minor fixes

--- modules/editors/nvim/lua/plugins/theme.lua
+++ modules/editors/nvim/lua/plugins/theme.lua
@@ -14,7 +14,7 @@ return {
       vim.g.nord_contrast = false
       vim.g.nord_borders = true
       vim.g.nord_disable_background = false
-      vim.g.nord_italic = true
+      vim.g.nord_italic = false
       vim.g.nord_uniform_diff_background = true
 			vim.g.nord_enable_sidebar_background = true
 			vim.g.nord_bold = true
