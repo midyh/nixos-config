164e0ab4e63043f91e6d17efeb6055f10f8a2668
midyHamdoun <midyan.hamdoun@gmail.com>
2023-07-15 06:23:11 +0300

    Fix typo

--- modules/theme/catppuccin-dark/wayland/notice/default.nix
+++ modules/theme/catppuccin-dark/wayland/notice/default.nix
@@ -1,17 +1,4 @@
 { config, pkgs, ... }:
- gtk3.extraConfig = {
-      gtk-xft-antialias = 0; # 1
-      gtk-xft-hinting = 1;
-      gtk-xft-hintstyle = "hintslight";
-      gtk-xft-rgba = "rgb";
-    };
-
-    gtk2.extraConfig = ''
-      gtk-xft-antialias = 0;
-      gtk-xft-hinting = 1;
-      gtk-xft-hintstyle = "hintslight";
-      gtk-xft-rgba = "rgb";
-    '';
 
 {
   services.mako = {
--- modules/theme/nord/wayland/notice/default.nix
+++ modules/theme/nord/wayland/notice/default.nix
@@ -1,19 +1,5 @@
 { config, pkgs, ... }:
 {
- gtk3.extraConfig = {
-      gtk-xft-antialias = 0; # 1
-      gtk-xft-hinting = 1;
-      gtk-xft-hintstyle = "hintslight";
-      gtk-xft-rgba = "rgb";
-    };
-
-    gtk2.extraConfig = ''
-      gtk-xft-antialias = 0;
-      gtk-xft-hinting = 1;
-      gtk-xft-hintstyle = "hintslight";
-      gtk-xft-rgba = "rgb";
-    '';
-
   services.mako = {
     font = "FiraCode Nerd Font";
     width = 256;
