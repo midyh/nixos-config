2f0484728a181039e4db5213e79bfaf6be2c1b99
midyh <midyan.hamdoun@gmail.com>
2023-09-23 18:28:39 +0300

    add bat instead of cat

--- modules/editors/nvim/options.lua
+++ modules/editors/nvim/options.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.opt.nu = true
-vim.opt.relativenumber = true
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
---
+++ modules/shell/fish/functions/cat.nix
@@ -0,0 +1,5 @@
+''
+  function cat
+    bat $argv
+  end
+''
